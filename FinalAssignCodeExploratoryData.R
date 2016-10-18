17c4533d80589cff7c9d61dd1d80cbd49a6cec20
James Jackson <jjacko.1623@gmail.com>
2016-10-18 19:35:23 +1100

    plot code finished

--- FinalAssignCodeExploratoryData.R
+++ FinalAssignCodeExploratoryData.R
@@ -1,7 +1,8 @@
 ##Final assignment - Exploratory data
 
 #set wd
-setwd("C:/Users/jjack_000/Desktop/Data Science JH course/Exploratory Data Analysis")
+#setwd("C:/Users/jjack_000/Desktop/Data Science JH course/Exploratory Data Analysis")
+setwd("C:/Users/Sophy/Desktop/JJ Coursera/Exploratory Data/ExData_Final")
 
 ##read in data
 NEI <- readRDS("summarySCC_PM25.rds")
@@ -108,8 +109,101 @@ SCC <- readRDS("Source_Classification_Code.rds")
 #ggplot(totTypeYrBaltEmmDF,aes(Year,TotalEmmissions))+geom_point(color="aquamarine4")+facet_wrap(~Type,ncol=2) #+ scale_y_continuous(name="Total Emissions (per Type)",limits = c(2500, 8500))
 
 
-#Plot 4
+##Plot 4
+
+#trim SCC data as will save on merge
+#trimSCC <- SCC[,c(1,3)]
+  
+#merge data
+#mData <- merge(NEI,trimSCC, by = c("SCC"), all.x = T)
 
 #check in short name for coal
-coalItems <- grep("Coal",SCC$Short.Name)
+#CoalItems <- grep("Coal",mData$Short.Name)
 
+#coalData <- mData[CoalItems,]
+
+#yearsData <- unique(NEI$year)
+
+#totEmm99 <- sum(coalData$Emissions[coalData$year == 1999])
+#totEmm02 <- sum(coalData$Emissions[coalData$year == 2002])
+#totEmm05 <- sum(coalData$Emissions[coalData$year == 2005])
+#totEmm08 <- sum(coalData$Emissions[coalData$year == 2008])
+
+#totEmm <- c(totEmm99,totEmm02,totEmm05,totEmm08)
+
+
+#yticks <- seq(350000,650000,100000)
+#plot(yearsData,totEmm,xlab = "Year",ylab = "Total Emmissions",xaxt="n")
+#axis(1,at = yearsData,labels = T)
+#axis(2,at = yticks,labels = yticks)
+#bestFit <- lm(totEmm ~ yearsData)
+#abline(bestFit, lwd=1.5)
+
+##So the total emmissions appears to be decreasing
+##This may be due to a number of things such as fewer recordings, not just the obvious result of lower emmissions
+
+
+##Plot 5
+
+#yearsData <- unique(NEI$year)
+
+#filter by Baltimore and On-road
+#baltData <- subset(NEI,fips == "24510")
+#RbaltData <- subset(baltData,type == "ON-ROAD")
+
+#totEmm99 <- sum(RbaltData$Emissions[RbaltData$year == 1999])
+#totEmm02 <- sum(RbaltData$Emissions[RbaltData$year == 2002])
+#totEmm05 <- sum(RbaltData$Emissions[RbaltData$year == 2005])
+#totEmm08 <- sum(RbaltData$Emissions[RbaltData$year == 2008])
+
+#totEmm <- c(totEmm99,totEmm02,totEmm05,totEmm08)
+
+#yticks <- seq(0,400,100)
+#plot(yearsData,totEmm,xlab = "Year",ylab = "Total Emmissions",xaxt="n")
+#axis(1,at = yearsData,labels = T)
+#axis(2,at = yticks,labels = yticks)
+#bestFit <- lm(totEmm ~ yearsData)
+#abline(bestFit, lwd=1.5)
+
+##So the total emmissions appears to be decreasing
+##This may be due to a number of things such as fewer recordings, not just the obvious result of lower emmissions
+
+
+##Plot 6
+
+#plot both Balt data and cali data
+
+yearsData <- unique(NEI$year)
+
+#filter by Baltimore and On-road
+baltData <- subset(NEI,fips == "24510")
+RbaltData <- subset(baltData,type == "ON-ROAD")
+
+totBaltEmm99 <- sum(RbaltData$Emissions[RbaltData$year == 1999])
+totBaltEmm02 <- sum(RbaltData$Emissions[RbaltData$year == 2002])
+totBaltEmm05 <- sum(RbaltData$Emissions[RbaltData$year == 2005])
+totBaltEmm08 <- sum(RbaltData$Emissions[RbaltData$year == 2008])
+
+totBaltEmm <- c(totBaltEmm99,totBaltEmm02,totBaltEmm05,totBaltEmm08)
+
+#filter by California and On-road
+calData <- subset(NEI,fips == "06037")
+RcalData <- subset(calData,type == "ON-ROAD")
+
+totCalEmm99 <- sum(RcalData$Emissions[RcalData$year == 1999])
+totCalEmm02 <- sum(RcalData$Emissions[RcalData$year == 2002])
+totCalEmm05 <- sum(RcalData$Emissions[RcalData$year == 2005])
+totCalEmm08 <- sum(RcalData$Emissions[RcalData$year == 2008])
+
+totCalEmm <- c(totCalEmm99,totCalEmm02,totCalEmm05,totCalEmm08)
+
+yticksBalt <- seq(0,400,100)
+yticksCal <- seq(3500,4500,200)
+
+par(mfrow = c(1,2))
+plot(yearsData,totBaltEmm,xlab = "Year",ylab = "Total Emmissions",xaxt="n",col = "red")
+axis(1,at = yearsData,labels = T)
+axis(2,at = yticksBalt,labels = yticksBalt)
+plot(yearsData,totCalEmm,xlab = "Year",ylab = "Total Emmissions",xaxt="n",col = "blue")
+axis(1,at = yearsData,labels = T)
+axis(2,at = yticksCal,labels = yticksCal)
